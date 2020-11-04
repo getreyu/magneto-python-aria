5c4f2c69a91a6fae7df076dac98558ff217a6ff4
sourav kundu <70445417+26souravk@users.noreply.github.com>
2020-11-04 15:15:23 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -13,8 +13,8 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading...📤"
-    STATUS_DOWNLOADING = "Downloading...📥"
+    STATUS_UPLOADING = "Uploading To Moviesbazz Cloud📤"
+    STATUS_DOWNLOADING = "Downloading 📥"
     STATUS_WAITING = "Queued...📝"
     STATUS_FAILED = "Failed 🚫. Cleaning download"
     STATUS_CANCELLED = "Cancelled ❎"
@@ -92,15 +92,15 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>Filename :</b> <code>{download.name()}</code>"
-            msg += f"\n<b>Status :</b> <i>{download.status()}</i>"
+            msg += f"<b>📁Filename :</b> <code>{download.name()}</code>"
+            msg += f"\n<b>🧲Status :</b> <i>{download.status()}</i>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
                 msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>" \
-                       f"\n<b>Downloaded :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}" \
-                       f"\n<b>Speed :</b> {download.speed()}, \n<b>ETA:</b> {download.eta()} "
+                       f"\n<b>✅Downloaded :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}" \
+                       f"\n<b>🚀Speed :</b> {download.speed()}, \n<b>⏳ETA:</b> {download.eta()} "
                 # if hasattr(download, 'is_torrent'):
                 try:
-                    msg += f"\n<b>Info :- Seeders:</b> {download.aria_download().num_seeders}" \
+                    msg += f"\n<b>ℹ️Info :- Seeders:</b> {download.aria_download().num_seeders}" \
                         f" & <b>Peers :</b> {download.aria_download().connections}"
                 except:
                     pass
