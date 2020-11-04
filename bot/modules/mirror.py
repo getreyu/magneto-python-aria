9ddd8db22cf5d744c32c3d5ea21981edac87e0ad
sourav kundu <70445417+26souravk@users.noreply.github.com>
2020-11-04 16:24:40 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -156,7 +156,7 @@ class MirrorListener(listeners.MirrorListeners):
             else:
                 uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
             if uname is not None:
-                msg += f'\n\n{uname} Your File Is Uploaded✅\n\n🚫 DO NOT SHARE ANY LINK PUBLICLY  🚫\n\n<b>💥POWERED BY : </b> @Moviesbazz'
+                msg += f'\n\n{uname} Your File Is Uploaded✅\n\n<b>🚫 DO NOT SHARE ANY LINK PUBLICLY 🚫</b>\n\n<b>💥POWERED BY : </b> @Moviesbazz'
             try:
                 fs_utils.clean_download(download_dict[self.uid].path())
             except FileNotFoundError:
